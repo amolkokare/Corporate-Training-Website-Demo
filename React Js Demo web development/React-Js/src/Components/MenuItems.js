0573413dac559154f836590dd7f2859987bab235
Yogesh Rupnawar <78634372+Yogesh49@users.noreply.github.com>
2021-04-07 10:16:55 +0530

    ReactJs file
    Home page with header navbar

---
+++ React Js Demo web development/React-Js/src/Components/MenuItems.js
@@ -0,0 +1,49 @@
+export const MenuItems =[
+    {
+        title:'Corporate Training',
+        path:'/corporate',
+        cName:'dropdown-link'
+    },
+    {
+        title:'Social Responsbility',
+        path:'/social',
+        cName:'dropdown-link'
+    },
+    {
+        title:'New Course',
+        path:'/corse',
+        cName:'dropdown-link'
+    },
+    {
+        title:'Manis Hiring',
+        path:'/manis',
+        cName:'dropdown-link'
+    }
+]
+export const Menu =[
+    {
+        title:'Corporate Training',
+        path:'/corporate',
+        cName:'dropdownc-link'
+    },
+    {
+        title:'Microsoft',
+        path:'/microsoft',
+        cName:'dropdownc-link'
+    },
+    {
+        title:'Cisco',
+        path:'/cisco',
+        cName:'dropdownc-link'
+    },
+    {
+        title:'SAP',
+        path:'/sap',
+        cName:'dropdownc-link'
+    },
+    {
+        title:'Motivational Training',
+        path:'/motivational',
+        cName:'dropdownc-link'
+    }
+]
