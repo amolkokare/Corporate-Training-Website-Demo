8575179277665a8eb116f3712740fe965dac5904
Shubham Chavan <78634533+shubh9040@users.noreply.github.com>
2021-04-07 10:15:00 +0530

    .net api file
    student databse created on sql server by using asp .net core

---
+++ .Net Api Demo website/.Net Api_Corp_Demo/Migrations/20210403070607_Initial Create.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Corporation_demo.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    CollegeName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Mobile = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Degree = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Specialization = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    DateOfBirth = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    PhotoFileName = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
