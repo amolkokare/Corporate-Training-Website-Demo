a473d597416c184e075df89ea7b4ceafebbae41d
ApurvaFlair <75774709+ApurvaFlair@users.noreply.github.com>
2021-04-08 13:50:27 +0530

    images
    client page

--- React Js Demo web development/React-Js/src/Components/pages/Home.js
+++ React Js Demo web development/React-Js/src/Components/pages/Home.js
@@ -23,8 +23,33 @@ export default function Home() {
             </div>
 
             <div className='ef'>
-                <img src='/img/r new.bmp' alt="ri"></img>
+                <img src={process.env.PUBLIC_URL+"/img/r new.bmp"} alt="ri"></img>
             </div>
+            <h1 className="heading">
+                <h3 >Our Clients</h3>
+                </h1>
+                <h1 className="heading1">
+                <img src={process.env.PUBLIC_URL+"/images/ls.png"}alt="/" width="100"></img>
+                </h1>
+            <div className="container" >
+            
+                    
+                <img src={process.env.PUBLIC_URL+"/images/img1.png"}alt="/" rounded ></img>
+                <img src={process.env.PUBLIC_URL+"/images/img2.png"}  alt="/"></img>
+                <img src={process.env.PUBLIC_URL+"/images/img3.png"}  alt="/"height="150"></img>
+                <img src={process.env.PUBLIC_URL+"/images/img4.png"} alt="/" height="150"></img>
+                <img src={process.env.PUBLIC_URL+"/images/img5.jpg"}  alt="/"></img>
+                <img src={process.env.PUBLIC_URL+"/images/img8.png"}  alt="/"></img>
+                <img src={process.env.PUBLIC_URL+"/images/img9.png"} alt="/"></img>
+                <img src={process.env.PUBLIC_URL+"/images/img10.png"} alt="/"></img>
+                <img src={process.env.PUBLIC_URL+"/images/img12.png"} alt="/"></img>
+                <img src={process.env.PUBLIC_URL+"/images/img6.jpg"} alt="/"></img>
+                <img src={process.env.PUBLIC_URL+"/images/img7.jpg"} alt="/"></img>
+                <img src={process.env.PUBLIC_URL+"/images/img13.png"} alt="/" height="100"></img>
+
+
+                
+</div>
         </>
     );
 }
