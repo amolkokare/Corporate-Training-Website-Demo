1d67a4e46ce1c1b9180cf11fe386d135f310c063
Shubham Chavan <78634533+shubh9040@users.noreply.github.com>
2021-04-08 12:21:15 +0530

    .net api
    .net api

---
+++ .Net Api Demo website/Corporation_demo/Corporation_demo/Controllers/SyllabusController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Corporation_demo.Models;
+
+namespace Corporation_demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SyllabusController : ControllerBase
+    {
+        private readonly CorporationDBContext _context;
+
+        public SyllabusController(CorporationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Syllabus
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Syllabus_c>>> GetSyllabus_cs()
+        {
+            return await _context.Syllabus_cs.ToListAsync();
+        }
+
+        // GET: api/Syllabus/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Syllabus_c>> GetSyllabus_c(int id)
+        {
+            var syllabus_c = await _context.Syllabus_cs.FindAsync(id);
+
+            if (syllabus_c == null)
+            {
+                return NotFound();
+            }
+
+            return syllabus_c;
+        }
+
+        // PUT: api/Syllabus/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSyllabus_c(int id, Syllabus_c syllabus_c)
+        {
+            if (id != syllabus_c.SyllabusId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(syllabus_c).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!Syllabus_cExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Syllabus
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Syllabus_c>> PostSyllabus_c(Syllabus_c syllabus_c)
+        {
+            _context.Syllabus_cs.Add(syllabus_c);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSyllabus_c", new { id = syllabus_c.SyllabusId }, syllabus_c);
+        }
+
+        // DELETE: api/Syllabus/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSyllabus_c(int id)
+        {
+            var syllabus_c = await _context.Syllabus_cs.FindAsync(id);
+            if (syllabus_c == null)
+            {
+                return NotFound();
+            }
+
+            _context.Syllabus_cs.Remove(syllabus_c);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool Syllabus_cExists(int id)
+        {
+            return _context.Syllabus_cs.Any(e => e.SyllabusId == id);
+        }
+    }
+}
---
+++ .Net Api Demo website/Corporation_demo/Corporation_demo/Migrations/20210406080042_Initial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Corporation_demo.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    username = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    password = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
