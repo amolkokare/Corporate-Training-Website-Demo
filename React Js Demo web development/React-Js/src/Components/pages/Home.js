483f4c5dd6961707fa66d39237f165f2585667c8
ApurvaFlair <75774709+ApurvaFlair@users.noreply.github.com>
2021-04-14 15:50:10 +0530

    update some page
    changes in home page

--- React Js Demo web development/React-Js/src/Components/pages/Home.js
+++ React Js Demo web development/React-Js/src/Components/pages/Home.js
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import '../../App.css';
-
+import '../../client.css';
+import '../../patner.css';
 export default function Home() {
     return (
         <>
@@ -29,27 +30,142 @@ export default function Home() {
                 <h3 >Our Clients</h3>
                 </h1>
                 <h1 className="heading1">
-                <img src={process.env.PUBLIC_URL+"/images/ls.png"}alt="/" width="100"></img>
+                <img src={process.env.PUBLIC_URL+"/images/ls.png"}alt="/" width="150"></img>
                 </h1>
-            <div className="container" >
+            <div className="client" >
             
                     
-                <img className='img1' src={process.env.PUBLIC_URL+"/images/img1.png"}alt="/" rounded></img>
-                <img className='img1' src={process.env.PUBLIC_URL+"/images/img2.png"}  alt="/"></img>
-                <img className='img1' src={process.env.PUBLIC_URL+"/images/img3.png"}  alt="/"height="150"></img>
-                <img className='img1' src={process.env.PUBLIC_URL+"/images/img4.png"} alt="/" height="150"></img>
-                <img className='img1' src={process.env.PUBLIC_URL+"/images/img5.jpg"}  alt="/"></img>
-                <img className='img1' src={process.env.PUBLIC_URL+"/images/img8.png"}  alt="/"></img>
-                <img className='img1' src={process.env.PUBLIC_URL+"/images/img9.png"} alt="/"></img>
-                <img className='img1' src={process.env.PUBLIC_URL+"/images/img10.png"} alt="/"></img>
-                <img className='img1' src={process.env.PUBLIC_URL+"/images/img12.png"} alt="/"></img>
-                <img className='img1' src={process.env.PUBLIC_URL+"/images/img6.jpg"} alt="/"></img>
-                <img className='img1' src={process.env.PUBLIC_URL+"/images/img7.jpg"} alt="/"></img>
-                <img className='img1'  src={process.env.PUBLIC_URL+"/images/img13.png"} alt="/" height="100"></img>
+            <div className="container">    
+            
 
+           <img src={process.env.PUBLIC_URL+"/images/img3.png"}  alt="/" width="140"></img>
+             
+             </div>
+             <div className="container1">
+             
+      
+                <img src={process.env.PUBLIC_URL+"/images/img4.png"}  alt="/"width="150"></img>
+           
+                </div>
+                <div className="container2">
+              
+    
+                <img src={process.env.PUBLIC_URL+"/images/img5.jpg"}  alt="/"width="80"></img>
+           
+                </div>
+                <div className="container3">
+            
+                <img src={process.env.PUBLIC_URL+"/images/img6.jpg"}  alt="/"width="80"></img>
+           
+                </div>
+                <div className="container4">
+            
 
+                 <img src={process.env.PUBLIC_URL+"/images/img7.png"}  alt="/" width="100"></img>
                 
-</div>
+                 </div> 
+                 <div className="container5">
+               
+                <img src={process.env.PUBLIC_URL+"/images/img8.png"}  alt="/"width="70"></img>
+                 
+                 </div> 
+                 <div className="container6">
+                
+                <img src={process.env.PUBLIC_URL+"/images/img9.png"}  alt="/"width="140"></img>
+                
+                 </div>
+                
+                 <div className="container7">
+                 <img src={process.env.PUBLIC_URL+"/images/img8.png"}  alt="/"width="70"></img>
+                 
+                 </div> 
+                 <div className="container8">
+                 <img src={process.env.PUBLIC_URL+"/images/img8.png"}  alt="/"width="70"></img>
+                 
+                 </div> 
+                 <div className="container9">
+                 <img src={process.env.PUBLIC_URL+"/images/img8.png"}  alt="/"width="70"></img>
+
+                </div>
+                </div>
+                
+        
+        
+        <h1 className="heading">
+            <h3 >Our partners</h3>
+            </h1>
+            <h1 className="heading1">
+            <img src={process.env.PUBLIC_URL+"/images/ls.png"}alt="/" width="150"></img>
+            </h1>   
+        <div className="patner">
+      
+            <div className="pcontainer">    
+            <a href = {"/AboutUs"}>
+
+           
+             <img src={process.env.PUBLIC_URL+"/images/img3.png"}  alt="/" width="140"></img>
+             </a>
+
+             </div>
+             <div className="pcontainer1">
+             <a href = {"/AboutUs"}>
+      
+                <img src={process.env.PUBLIC_URL+"/images/img4.png"}  alt="/"width="150"></img>
+            </a>
+                </div>
+                <div className="pcontainer2">
+                <a href = {"/AboutUs"}>
+    
+                <img src={process.env.PUBLIC_URL+"/images/img5.jpg"}  alt="/"width="80"></img>
+            </a>
+                </div>
+                <div className="pcontainer3">
+                <a href = {"/AboutUs"}>
+
+                <img src={process.env.PUBLIC_URL+"/images/img6.jpg"}  alt="/"width="80"></img>
+           </a>
+                </div>
+                <div className="pcontainer4">
+                <a href = {"/AboutUs"}>
+
+                 <img src={process.env.PUBLIC_URL+"/images/img7.png"}  alt="/" width="100"></img>
+                     </a>  
+                 </div> 
+                 <div className="pcontainer5">
+                 <a href = {"/AboutUs"}>
+
+      
+                <img src={process.env.PUBLIC_URL+"/images/img8.png"}  alt="/"width="70"></img>
+                 </a>
+                 </div> 
+                 <div className="pcontainer6">
+                 <a href = {"/AboutUs"}>
+         
+                <img src={process.env.PUBLIC_URL+"/images/img9.png"}  alt="/"width="140"></img>
+                </a>
+                 </div>
+                <div className="pcontainer7">
+                <a href = {"/AboutUs"}>
+        
+                 <img src={process.env.PUBLIC_URL+"/images/img10.png"}  alt="/"width="150"></img>
+                </a>
+                 </div>
+                <div className="pcontainer8">
+                <a href = {"/AboutUs"}>
+       
+                  <img src={process.env.PUBLIC_URL+"/images/img11.jpg"}  alt="/"width="100"></img>
+                </a>
+                 </div>
+                 <div className="pcontainer9">
+                 <a href = {"/AboutUs"}>
+       
+                 <img src={process.env.PUBLIC_URL+"/images/img12.png"}  alt="/"width="140"></img>
+                 </a>
+  
+                 </div>
+           </div>
+           
+           
         </>
     );
 }
